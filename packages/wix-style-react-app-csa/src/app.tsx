8e04292cb2c121e66683702305a45712f741cd40
TalZa1810 <24567980+TalZa1810@users.noreply.github.com>
2021-08-22 12:40:28 +0300

    update app

--- packages/wix-style-react-app-csa/src/app.tsx
+++ packages/wix-style-react-app-csa/src/app.tsx
@@ -1,7 +1,6 @@
 import type React from 'react';
 import { Button } from 'wix-style-react';
 import { st, classes } from './app.st.css';
-import { Header } from './header';
 
 export interface AppProps {
     className?: string;
@@ -10,7 +9,6 @@ export interface AppProps {
 export const App: React.VFC<AppProps> = ({ className }) => {
     return (
         <main className={st(classes.root, className)}>
-            <Header className={classes.header} />
             <Button>Hello !</Button>
         </main>
     );
