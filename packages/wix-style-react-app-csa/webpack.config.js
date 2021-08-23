aa413639790c96d5253afd85504739a102f229e0
urig <urig@wix.com>
2021-08-23 11:36:41 +0300

    webpack config - modify devServer

--- packages/wix-style-react-app-csa/webpack.config.js
+++ packages/wix-style-react-app-csa/webpack.config.js
@@ -6,6 +6,10 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+      },
     module: {
         rules: [
             {
