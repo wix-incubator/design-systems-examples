76a676a8bcf7b65e32a56b1abaad60f410930fca
urig <urig@wix.com>
2021-08-24 16:47:20 +0300

    fix build

--- packages/wix-style-react-app-csa/src/ProductsPage/ProductsPage.tsx
+++ packages/wix-style-react-app-csa/src/ProductsPage/ProductsPage.tsx
@@ -19,8 +19,6 @@ import Delete from 'wix-ui-icons-common/Delete';
 import { useHistory } from 'react-router-dom';
 import { Context, Product } from '../Context/context';
 
-BREAK BUILD !!! 
-
 const ProductsPage: React.FC = () => {
     const history = useHistory();
     const { products, addProduct, removeProduct } = useContext(Context);
