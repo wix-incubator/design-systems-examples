a8c05ae2bf2681ca666aa87f164ec5c9b5203fda
urig-wix <73787639+urig-wix@users.noreply.github.com>
2021-08-25 16:34:30 +0300

    Add webpack (#8)
    * add webpack from scratch config
    
    * add webpack from scratch config
    
    * more webpack installation

---
+++ packages/wix-style-react-webpack/src/ProductPage/ProductsPage.tsx
@@ -0,0 +1,7 @@
+import { Button } from 'wix-style-react';
+const ProductsPage = (): any => <div>
+    <Button>Stylable app</Button>
+    Products
+    </div>;
+
+export default ProductsPage;
---
+++ packages/wix-style-react-webpack/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ProductsPage from "./ProductPage/ProductsPage";
+
+ReactDOM.render(
+  <ProductsPage />,
+  document.body.appendChild(document.createElement("div"))
+);
